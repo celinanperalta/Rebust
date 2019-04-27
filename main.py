b49fc4cb9bf8164460d82f535836ba79ea326ee9
CelinaPeralta <celper19@bergen.org>
2019-04-27 10:10:31 -0400

    datamuse

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
+from datamuse import datamuse
 
 app = Flask(__name__)
-
+api = datamuse.Datamuse()
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
+@app.route("/rhymes/<string:word>/")
+def rhymes(word):
+    word_rhy = api.words(sl=word, max=5)
+    return render_template('rhymes.html',word_rhy=word_rhy)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
