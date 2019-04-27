a5af3ca64df306e108cb535ab30387e1fc58025e
CelinaPeralta <celper19@bergen.org>
2019-04-27 15:12:49 -0400

    clarifai bro

--- rebust.py
+++ rebust.py
@@ -1,32 +1,33 @@
 from datamuse import datamuse
 import re
 
-api = datamuse.Datamuse()
+class Rebust:
 
-#given an array of strings and operators, concat individual rebus word
-def concat_word(word):
-    str = ""
-    for i in range(0, len(word)):
-       if word[i] == "-":
-           str = str.replace(word[i+1], '', 1)
-           i += 2
-       elif word[i] != "+" and word[i-1] != "-":
-           str += word[i]
+    MAX_RESULTS = 20
 
-    return str
+    def __init__(self):
+        self.api = datamuse.Datamuse()
 
-def get_sl(word):
-    print("Similar sound to: " + word)
-    for w in api.words(sl=word, v='enwiki', max=20):
-        print(w['word'])
-    print("---------------------------------------")
+    #given an array of strings and operators, concat individual rebus word
+    def concat_word(self, rebus):
+        word = re.split(" ", rebus)
+        str = ""
+        for i in range(0, len(word)):
+           if word[i] == "-":
+               str = str.replace(word[i+1], '', 1)
+               i += 2
+           elif word[i] != "+" and word[i-1] != "-":
+               str += word[i]
+        return str
 
-# def image_to_word(img):
+    def get_sounds_like(self, word):
+        print("Similar sound to: " + word)
+        for w in self.api.words(sl=word, v='enwiki', max=self.MAX_RESULTS):
+            print(w['word'])
+        print("---------------------------------------")
 
+    def image_to_word(self, img):
+        return -1
 
-str = re.split(" ", input("Enter a rebus word: "))
-print(str)
-
-get_sl(concat_word(str))
 
 
--- tensor_test.py
+++ tensor_test.py
@@ -10,9 +10,16 @@ import PIL.Image as Image
 
 from tensorflow.keras import layers
 
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+init_op = tf.global_variables_initializer()
+
+sess = tf.Session()
+sess.run(init_op)
+
 data_root = tf.keras.utils.get_file(
-  'flower_photos','https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz',
-   untar=True)
+  'flower_photos','https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz',untar=True)
 image_generator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1/255)
 image_data = image_generator.flow_from_directory(str(data_root))
 for image_batch,label_batch in image_data:
@@ -34,27 +41,35 @@ classifier_layer = layers.Lambda(classifier, input_shape = IMAGE_SIZE+[3])
 classifier_model = tf.keras.Sequential([classifier_layer])
 classifier_model.summary()
 
+
 image_data = image_generator.flow_from_directory(str(data_root), target_size=IMAGE_SIZE)
 for image_batch,label_batch in image_data:
   print("Image batch shape: ", image_batch.shape)
   print("Labe batch shape: ", label_batch.shape)
   break
 
+
 grace_hopper = tf.keras.utils.get_file('image.jpg',
                                      'https://storage.googleapis.com/download.tensorflow.org/example_images/grace_hopper.jpg')
 grace_hopper = Image.open(grace_hopper).resize(IMAGE_SIZE)
 grace_hopper
 
+
 grace_hopper = np.array(grace_hopper) / 255.0
 grace_hopper.shape
+
 result = classifier_model.predict(grace_hopper[np.newaxis, ...])
 result.shape
 predicted_class = np.argmax(result[0], axis=-1)
 predicted_class
+
 labels_path = tf.keras.utils.get_file('ImageNetLabels.txt',
                                     'https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt')
 imagenet_labels = np.array(open(labels_path).read().splitlines())
 plt.imshow(grace_hopper)
 plt.axis('off')
+
+
 predicted_class_name = imagenet_labels[predicted_class]
 _ = plt.title("Prediction: " + predicted_class_name)
+
