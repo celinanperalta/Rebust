72ebc118f6f763be43defa575269fba1754b01ef
CelinaPeralta <celper19@bergen.org>
2019-04-27 09:26:41 -0400

    hola

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
