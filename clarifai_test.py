795bdb26fd41c8afe88e36504fa9f02a06ca0b86
CelinaPeralta <celper19@bergen.org>
2019-04-27 15:23:36 -0400

    rebust

--- clarifai_test.py
+++ clarifai_test.py
@@ -1,10 +1,10 @@
-from clarifai.rest import ClarifaiApp
-import json
+from rebust import Rebust
 
-app = ClarifaiApp(api_key='efac6c485194474790aa9732ed895aa2')
-model = app.public_models.general_model
-response = model.predict_by_url('https://samples.clarifai.com/metro-north.jpg')
-results = response["outputs"][0]["data"]["concepts"]
-for i in results:
-    print(i["name"])
-    print(i["value"])
+rebust = Rebust()
+
+img = input("Enter URL or image path: ")
+
+res = rebust.get_image_predictions(img)
+
+for i in res:
+    print(i)
--- rebust.py
+++ rebust.py
@@ -1,4 +1,5 @@
 from datamuse import datamuse
+from clarifai.rest import ClarifaiApp
 import re
 
 class Rebust:
@@ -7,6 +8,8 @@ class Rebust:
 
     def __init__(self):
         self.api = datamuse.Datamuse()
+        self.app = ClarifaiApp(api_key='efac6c485194474790aa9732ed895aa2')
+        self.model = self.app.public_models.general_model
 
     #given an array of strings and operators, concat individual rebus word
     def concat_word(self, rebus):
@@ -26,8 +29,22 @@ class Rebust:
             print(w['word'])
         print("---------------------------------------")
 
-    def image_to_word(self, img):
-        return -1
+    def get_image_predictions(self, img):
+        regex = re.compile(
+            r'^(?:http|ftp)s?://'  # http:// or https://
+            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+            r'localhost|'  # localhost...
+            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+            r'(?::\d+)?'  # optional port
+            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
+        response = None
 
+        if re.match(regex, img) is not None:
+            response = self.model.predict_by_url(img)
+        else:
+            response = self.model.predict_by_filename(img)
 
+        results = response["outputs"][0]["data"]["concepts"]
+
+        return results
