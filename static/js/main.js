9e03a3d69ce7c838a0cbf8d048b1d4b86473b8d4
Anand Shetler <anandshetler@gmail.com>
2019-04-27 19:34:52 -0400

    uploads

--- static/js/main.js
+++ static/js/main.js
@@ -1,19 +1,36 @@
-var form = $("#rebus_form")
-var sizeTag = form.find("input[name='size']")
+var form
+var sizeTag
+$(window).on('load', function() {
+    form = $("#rebus_form")
+    sizeTag = $("#size")
+})
 function append_img_input() {
     var sizeStr = sizeTag.val()
     var newSize = parseInt(sizeStr.substring(sizeStr.indexOf("-")+1))+1
     sizeTag.val(sizeStr.substring(0,sizeStr.indexOf("-"))+"-"+newSize)
+    if (newSize!=1)
+        form.append("<p style='display:inline-block'>+</p>")
     form.append("<input type = \"file\" name=\""+sizeTag.val()+"\" multiple=\"false\" autocomplete=\"off\" required/>")
 }
 function append_text_input() {
     var sizeStr = sizeTag.val()
     var newSize = parseInt(sizeStr.substring(sizeStr.indexOf("-")+1))+1
     sizeTag.val(sizeStr.substring(0,sizeStr.indexOf("-"))+"-"+newSize)
+    if (newSize!=1)
+        form.append("<p style='display:inline-block'>+</p>")
     form.append("<input type = \"text\" name=\""+sizeTag.val()+"\" required/>")
+}
+function append_url_input() {
+    var sizeStr = sizeTag.val()
+    var newSize = parseInt(sizeStr.substring(sizeStr.indexOf("-")+1))+1
+    sizeTag.val(sizeStr.substring(0,sizeStr.indexOf("-"))+"-"+newSize)
+    if (newSize!=1)
+        form.append("<p style='display:inline-block'>+</p>")
+    form.append("<input type = \"url\" name=\""+sizeTag.val()+"\" required/>")
 }
 function add_word() {
     var sizeStr = sizeTag.val()
     var newSize = parseInt(sizeStr.substring(0,sizeStr.indexOf("-")))+1
     sizeTag.val(newSize+sizeStr.substring(sizeStr.indexOf("-")))
+    form.append("<br>")
 }
