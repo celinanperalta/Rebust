b462536858e25376767dfd0b2ab237f608afc139
nicolegerzon813 <nicolegerzon813@gmail.com>
2019-04-27 15:34:43 -0400

    dum shit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask import Flask, flash, redirect, render_template, request, url_for, session, abort
 from datamuse import datamuse
+from models import Info
 from werkzeug.utils import secure_filename
 import logging
 import os
@@ -20,9 +21,15 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route("/")
+@app.route("/",  methods =[ 'GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        data = Info(request.form['file'])
+        db.session.add(data)
+        db.session.commit()
+        return redirect(url_for('index'))
+    else:
+        return render_template('index.html')
 
 @app.route("/rhymes/<string:word>/")
 def rhymes(word):
@@ -35,12 +42,18 @@ def upload_file():
 
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_files():
-    if 'file' not in request.files:
-        flash('ur dum')
-        return redirect('/upload')
-    filename = secure_filename(file.filename)
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    flash('file uploaded succcccccessfully bish')
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('ur dum')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash('file uploaded succcccccessfully bish')
     return redirect('/upload')
 
 if __name__ == '__main__':
